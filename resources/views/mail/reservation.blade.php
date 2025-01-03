6b4a01de10914e9520ad4c548dda4738317853b3
leassi <kamgadevop@gmail.com>
2025-01-03 20:49:02 +0100

    mis a jour contenue de l'email

---
+++ resources/views/mail/reservation.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmation de Réservation</title>
+</head>
+<body>
+    <h1>Confirmation de Réservation Chez Jet Hotel</h1>
+    <p>Merci pour votre réservation. Veuillez trouver ci-joint la facture de votre réservation.</p>
+</body>
+</html>
